678918792e7b1b88e65c5163d14596cf8ade18ee
SAKSHISAAHU <sakshisahu060@gmail.com>
2025-02-20 22:56:49 +0530

    commit

---
+++ html css js/js/jsIterations/while.js
@@ -0,0 +1,23 @@
+// while loop
+// checks the condition first before executing the code inside the loop.
+let num=0
+while (num<=5) {
+    console.log(`number is: ${num}`)
+    num++
+}
+
+let myarr=['apple','mango','banana']
+let arr=0
+while (arr < myarr.length) {
+    console.log(`value ${myarr[arr]}`)
+    arr++
+    
+}
+
+// do-while loop
+// loop executes the code at least once, even if the condition is false.
+let num1=0
+do {
+    console.log(`value is: ${num1}`)
+    num1++
+} while (num1<=5);
